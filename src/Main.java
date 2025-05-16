6ef8712b2356f03564b5e5722dc6fa432c650ebb
Esrom Tekle <kidaneesrom34@gmail.com>
2025-05-16 10:31:19 -0700

    add this

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,71 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+package com.yearup.dealership;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+/**
+ * Abstract class representing a vehicle contract
+ * Contains common fields and methods for all contract types
+ */
+public abstract class Contract {
+    private String date;
+    private String customerName;
+    private String customerEmail;
+    private Vehicle vehicle;
+
+    /**
+     * Constructor for Contract class
+     * @param date Date of the contract
+     * @param customerName Name of the customer
+     * @param customerEmail Email of the customer
+     * @param vehicle Vehicle being sold or leased
+     */
+    public Contract(String date, String customerName, String customerEmail, Vehicle vehicle) {
+        this.date = date;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicle = vehicle;
     }
+
+    // Getters and setters
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    /**
+     * Abstract method to calculate the total price of the contract
+     * @return the total price
+     */
+    public abstract double getTotalPrice();
+
+    /**
+     * Abstract method to calculate the monthly payment
+     * @return the monthly payment amount
+     */
+    public abstract double getMonthlyPayment();
 }
