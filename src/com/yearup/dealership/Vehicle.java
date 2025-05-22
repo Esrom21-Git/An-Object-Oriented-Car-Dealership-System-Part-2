e3fbfe76343beb36a0890bbe0803821c3bd96454
Esrom Tekle <kidaneesrom34@gmail.com>
2025-05-22 09:52:41 -0700

    git pull

---
+++ src/com/yearup/dealership/Vehicle.java
@@ -0,0 +1,6 @@
+package com.yearup.dealership;
+
+public class Vehicle {
+    public Vehicle(int vin, int year, String make, String model, String vehicleType, String color, int odometer, double price) {
+    }
+}
